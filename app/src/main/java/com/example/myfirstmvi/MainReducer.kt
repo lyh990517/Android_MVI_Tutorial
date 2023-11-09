99180dfe991cd6e0f51e92cf96498cbd65e2999b
lyh990517 <lyh990517@naver.com>
2023-11-09 23:29:03 +0900

    사용하는 state 수정

--- app/src/main/java/com/example/myfirstmvi/MainReducer.kt
+++ app/src/main/java/com/example/myfirstmvi/MainReducer.kt
@@ -4,11 +4,11 @@ class MainReducer(state: MainState) : Reducer<MainState, MainEvent>(state) {
     override fun reduce(oldState: MainState, event: MainEvent) {
         when (event) {
             is MainEvent.Increase -> {
-                setState(oldState.copy(data = uiState.value.data + 1))
+                setState(oldState.copy(data = oldState.data + 1))
             }
 
             is MainEvent.Decrease -> {
-                setState(oldState.copy(data = uiState.value.data - 1))
+                setState(oldState.copy(data =  oldState.data - 1))
             }
         }
     }
